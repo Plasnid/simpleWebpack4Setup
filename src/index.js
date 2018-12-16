1ec5a5aee19a55e6a1b05593a269d6d402058ebb
Stuart Sackler <stuart.sackler@gmail.com>
2018-12-16 01:20:20 -0500

    a simple webpack and babel setup for es6

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// https://www.valentinog.com/blog/webpack-tutorial/
+console.log(`golly gee!  I'm a start point`);
+const arr = [1,2,3];
+const heyArr = () => console.log(...arr);
+window.heyArr = heyArr;
