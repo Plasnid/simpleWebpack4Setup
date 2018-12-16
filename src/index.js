eff866765a0ee21d26c98816407575257ad81157
Stuart Sackler <stuart.sackler@gmail.com>
2018-12-16 14:48:33 -0500

    handles html, js and css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import App from "./App";
+import style from "./main.css";
 // https://www.valentinog.com/blog/webpack-tutorial/
+
+
 console.log(`golly gee!  I'm a start point`);
 const arr = [1,2,3];
 const heyArr = () => console.log(...arr);
